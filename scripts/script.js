310b58ae345d58b25ddfe028026354139765b1f6
kessiamilena <kessia.milena124@yahoo.com>
2026-05-29 14:28:39 -0300

    :sparkles: feat: Resolução completa do exercício avançado - listagem de itens

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,36 @@
-const botaoAdicionar = document.getElementById("botao-adicionar");
-const lista = document.getElementById("lista");
+
+const botaoAdicionar = document.getElementById("botao-adicionar")
+const lista = document.getElementById("lista")
+const inputTarefa = document.getElementById("input-tarefas")
+const botaoLimpar = document.getElementById("botao-limpar")
 
 botaoAdicionar.addEventListener("click", () => {
 
-    // criar o item (elemento li filho do elemento ul)
-    // cria uma tag li, mas ainda não usa ela
-    const li = document.createElement("li"); 
+    if(inputTarefa.value.trim() === "") {
+        alert("Digite uma tarefa!");
+        return; // serve para quebrar e não executar mais nada pra baixo dessa função.
+    }
 
-    // criar o texto (elemento p)
+    // cria a tag <li> mas ainda não usa ela
+    const li = document.createElement("li");
+
+    // o elemento <p> recebe o valor digitado no input 
     const texto = document.createElement("p");
-    texto.innerText = "Novo item";
+    texto.innerText = inputTarefa.value;
 
-    // criar botão de lixeira
+    // criando o botão de lixeira
     const botaoLixeira = document.createElement("button");
     botaoLixeira.innerText = "🗑️"; // windows + "." abre os emojis
 
     botaoLixeira.style.border = "none"; // tira a borda do botão de lixeira
-    botaoLixeira.style.cursor = "pointer"; // quando passa o mouse em cima, vira mãozinha
-    botaoLixeira.style.backgroundColor = "none"; // retira a cor de fundo
+    botaoLixeira.style.cursor = "pointer" // trocar setinha por mãozinha
+    botaoLixeira.style.backgroundColor = "none" // retira a cor de fundo
 
-    // ação de remover o elemento da lista
     botaoLixeira.addEventListener("click", () => {
-        li.remove();
+        // li.remove(); // remove a tarefa
+
+        // risca a tarefa
+        texto.style.textDecoration = "line-through"
     })
 
     li.appendChild(texto);
@@ -29,4 +38,10 @@ botaoAdicionar.addEventListener("click", () => {
 
     lista.appendChild(li);
 
+    inputTarefa.value = ""
 })
+
+botaoLimpar.addEventListener("click", () => {
+    lista.innerHTML = "";
+})
+
